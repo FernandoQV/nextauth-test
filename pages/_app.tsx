66d5d8bc74298ec676550e135418e4dc8c1ae6c6
quintaro <octavius.sys@gmail.com>
2022-05-16 23:49:12 -0500

    feat:protected api with getSession

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const {session,...rest} = pageProps
+  return (
+    <SessionProvider session={session}>
+      <Component {...rest} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/react"
 
 type Data = {
-  name: string
+  name?: string
+  message?:string
 }
 
-export default function handler(
-  req: NextApiRequest,
+export default async function handler(
+  req:NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+const session=await getSession({req})
+console.log(session);
+  if(req.method==='GET'){
+    res.status(200).json({ name: "John Doe" })
+  }
+  if(req.method==='POST' && session){
+    res.status(201).json({message:"todo ok"})
+  }else{
+    res.status(201).json({message:"todo out"})
+
+  }
 }
